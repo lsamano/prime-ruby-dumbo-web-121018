7d1441e46154d39e2582a4c5a7d577a704f3887b
Leizl Samano <leizl.samano@gmail.com>
2018-12-02 04:43:21 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(integer)
+  array = []
+  for number in 1..integer do
+    array << number
+    number += 1
+  end
+
+  array.each do |number|
+    if integer % number != 0
+      false
+    else
+      true
+    end
+  end
+end
