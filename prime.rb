3f365068b3e991432c50d4a6020db09a2e22fe54
Leizl Samano <leizl.samano@gmail.com>
2018-12-02 04:57:16 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+def prime?(integer)
+  if integer <= 0 || integer == 1
+    return false
+  end
+    array = []
+    for number in 2..integer-1 do
+      array << number
+      number += 1
+    end
+
+    array.each do |number|
+      if integer % number == 0
+        return false
+      end
+    end
+    true
+end
